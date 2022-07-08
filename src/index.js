90cf8acfe6a6aad5bdffba277047840e75645ffd
goosewobbler <goosewobbler@pm.me>
2022-07-08 08:16:05 +0100

    rework origin setting

--- src/index.js
+++ src/index.js
@@ -6,9 +6,17 @@ const provider = ethProvider('ws://127.0.0.1:1248?identity=frame-extension')
 const subs = {}
 const pending = {}
 
-const getOrigin = url => {
-  const path = url.split('/')
-  return path[0] + '//' + path[2]
+const getOrigin = (tab = {}, sender = {}, method) => {
+  const originFromUrl = (url) => {
+    const path = url.split('/')
+    return `${path[0]}//${path[2]}`
+  }
+
+  if (method === 'embedded_action_res') {
+    return undefined
+  }
+
+  return originFromUrl(tab.url || sender.tab?.url || sender.url)
 }
 
 chrome.browserAction.setPopup({ popup: 'settings.html' })
@@ -105,7 +113,7 @@ chrome.runtime.onMessage.addListener(async (payload, sender, sendResponse) => {
     id,
     method,
     params,
-    __frameOrigin: getOrigin((tab || {}).url || sender.url),
+    __frameOrigin: getOrigin(tab, sender, method),
     __extensionConnecting: payload.__extensionConnecting
   }
 
