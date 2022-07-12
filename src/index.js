733b2720110e038e2c5350ecfbc5fba71b5983dc
goosewobbler <432005+goosewobbler@users.noreply.github.com>
2022-07-12 11:41:45 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ const provider = ethProvider('ws://127.0.0.1:1248?identity=frame-extension')
 const subs = {}
 const pending = {}
 
+const originFromUrl = (url) => {
+  const path = url.split('/')
+  return `${path[0]}//${path[2]}`
+}
 const getOrigin = (tab = {}, sender = {}, method) => {
-  const originFromUrl = (url) => {
-    const path = url.split('/')
-    return `${path[0]}//${path[2]}`
-  }
-
   if (method === 'embedded_action_res') {
     return undefined
   }
