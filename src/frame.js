402d61979d142395e9d39dcf6a262a15b93ef519
goosewobbler <goosewobbler@pm.me>
2023-03-15 17:12:46 +0000

    use object.defineProperty to set window.ethereum

--- src/frame.js
+++ src/frame.js
@@ -119,7 +119,11 @@ if (mmAppear) {
   }
 }
 
-window.ethereum = provider
+Object.defineProperty(window, 'ethereum', {
+  value: provider,
+  writable: true,
+  configurable: false
+})
 
 shimWeb3(window.ethereum, mmAppear)
 
@@ -129,7 +133,11 @@ const embedded = {
 
 document.addEventListener('readystatechange', (e) => {
   if (document.readyState === 'interactive') {
-    window.ethereum = provider
+    Object.defineProperty(window, 'ethereum', {
+      value: provider,
+      writable: true,
+      configurable: false
+    })
   }
 })
 
