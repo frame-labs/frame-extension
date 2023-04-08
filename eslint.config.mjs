833a66c9b36c1355aa51dd882b342bbf02ca09cb
goosewobbler <goosewobbler@protonmail.com>
2023-04-08 12:31:06 +0100

    update comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,10 @@ import prettierConfig from 'eslint-config-prettier'
 import frameLabsConfig from '@framelabs/config/eslint'
 
 export default [
+  // TODO: address linting errors and enable base config
   //...frameLabsConfig,
   // ---
-  // any repo-specific config here
+  // repo-specific config here
   // ---
   prettierConfig
 ]
